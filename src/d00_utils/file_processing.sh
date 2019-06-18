303f187ef21368c7008cb9fc9a310d866b6ea37e
wiebket <wiebke.toussaint@gmail.com>
2019-06-18 17:30:39 +0000

    added file description

--- src/d00_utils/file_processing.sh
+++ src/d00_utils/file_processing.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# description:	This shell script processes Xcelera_tablas database csv files so that they can be recreated in postgres.
+# 		The following processing steps are performed:
+# 		1) The last row of each file is removed, as it contains only NAN values (TODO: should rather use grep to find NAN rows)
+# 		2) File encoding is changed from IS08859 to UTF8
+# author:	wt
+# date: 	18 June 2019
+# usage:	./file_processing.sh
+# notes:	chmod +x file_processing.sh (make executable to run)
+#=====================================================================
 
 read -p "Specify path to raw data files: " RAW_DATA_DIR
 CLEAN_DATA_DIR=$HOME/data/Xcelera_tablas_clean
