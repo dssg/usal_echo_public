a37192f18d0f5aedf2076adbfa9d88c845d181ca
Yoni Nachmany <yoninachmany@gmail.com>
2019-07-02 16:35:34 +0000

    utils to work with db, tested in eda branch

---
+++ src/d01_data/db.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import sqlalchemy
+import yaml
+
+
+def get_db_config_path():
+    """
+    Get path to local configuration file with database credentials
+    
+    :return: path to config file
+    """
+    # TODO: document in README
+    return "../conf/local/db.yaml"
+
+
+def get_db_config(path):
+    """
+    Get configuration object with database credentials
+    
+    :param path: path to config file
+    :return: config object
+    """
+    with open(path) as f:
+        conf = yaml.safe_load(f)
+
+    return conf
+
+
+def get_sqlalchemy_connection(conf):
+    """
+    Get SQLAlchemy Engine for database
+    
+    :param conf: config object
+    :return: database connection
+    """
+    connection_string = "postgresql://{}:{}@{}/{}".format(
+        conf["user"], conf["pw"], conf["host"], conf["DB"]
+    )
+    conn = sqlalchemy.create_engine(connection_string)
+
+    return conn
+
+
+def read_table(conn, name):
+    """
+    Get dataframe from SQL table
+    
+    :param conn: database connection
+    :param name: table name
+    :return: dataframe of table
+    """
+    query = f"select * from {name}"
+    return pd.read_sql(query, conn)
