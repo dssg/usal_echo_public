8397f6f409315d5bf3255212defded29a5d68513
Yoni Nachmany <yoninachmany@gmail.com>
2019-07-02 16:33:55 +0000

    database utils restructured

--- src/d01_data/db.py
+++ src/d01_data/db.py
@@ -3,10 +3,36 @@ import sqlalchemy
 import yaml
 
 
-def get_sqlalchemy_engine():
-    with open("../conf/local/db.yaml") as f:
+def get_db_config_path():
+    """
+    Get path to local configuration file with database credentials
+    
+    :return: path to config file
+    """
+    # TODO: document in README
+    return "../conf/local/db.yaml"
+
+
+def get_db_config(path):
+    """
+    Get configuration object with database credentials
+    
+    :param path: path to config file
+    :return: config object
+    """
+    with open(path) as f:
         conf = yaml.safe_load(f)
 
+    return conf
+
+
+def get_sqlalchemy_connection(conf):
+    """
+    Get SQLAlchemy Engine for database
+    
+    :param conf: config object
+    :return: database connection
+    """
     connection_string = "postgresql://{}:{}@{}/{}".format(
         conf["user"], conf["pw"], conf["host"], conf["DB"]
     )
@@ -15,7 +41,13 @@ def get_sqlalchemy_engine():
     return conn
 
 
-def read_table(name):
+def read_table(conn, name):
+    """
+    Get dataframe from SQL table
+    
+    :param conn: database connection
+    :param name: table name
+    :return: dataframe of table
+    """
     query = f"select * from {name}"
-    conn = get_sqlalchemy_engine()
     return pd.read_sql(query, conn)
