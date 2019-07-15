54bcc24bc6c0f8e7ef9fadc9108477f2e32fbcf8
Dave Van Veen <davemvanveen@gmail.com>
2019-07-15 14:32:41 +0000

    updated import statements

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from d01_data import ingestion_dcm, ingestion_xtdb
 from d02_intermediate import clean_dcm, clean_xtdb
 from d03_classification import filter_views
 
-from d07_luigi import run_luigi
+#from d07_luigi import run_luigi
 
 if __name__ == '__main__':
     filter_views.filter_by_views()
