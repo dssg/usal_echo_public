9de660d12bf1cf087acfcdc0c95d93cdbd9ce565
Dave Van Veen <davemvanveen@gmail.com>
2019-07-15 18:30:19 +0000

    comments added v1

--- src/main.py
+++ src/main.py
@@ -4,13 +4,12 @@ Script for running full pipeline
 
 import os
 
-from d00_utils import db_utils, s3_utils
-from d01_data import ingestion_dcm, ingestion_xtdb
-from d02_intermediate import clean_dcm, clean_xtdb
-from d03_classification import filter_views
-
+#from d00_utils import db_utils, s3_utils
+#from d01_data import ingestion_dcm, ingestion_xtdb
+#from d02_intermediate import clean_dcm, clean_xtdb
+#from d03_classification import filter_views
 #from d07_luigi import run_luigi
 
 
 if __name__ == '__main__':
-    os.system('luigi --module main --of Pipeline')
+    os.system('luigi --module main --of Pipeline') # path issues? i.e. Pipeline() defined in d07
