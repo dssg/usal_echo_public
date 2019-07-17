e21564235e2856ea2a75fe59c877fe3d25efd390
Dave Van Veen <davemvanveen@gmail.com>
2019-07-17 15:36:32 +0000

    minor updates as of m1 dataset creation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from d00_utils import db_utils, s3_utils
-from d01_data import ingestion_dcm, ingestion_xtdb
+from d01_data import ingestion_dcm, ingestion_xtdb, master_list
 from d02_intermediate import clean_dcm, clean_xtdb
-from d03_classification import filter_views
+from d03_classification import filter_views, unlabeled_instances
 
 #from d07_luigi import run_luigi
 
 if __name__ == '__main__':
-    filter_views.filter_by_views()
-
+    #filter_views.filter_by_views()
+    #master_list.create_master_instance_list()
+    unlabeled_instances.create_unlabeled_instances()
