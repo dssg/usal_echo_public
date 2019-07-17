59af0a5d0408ac5045122c0891454c36ac333729
courtneyjean84 <courtney_irwin@hotmail.com>
2019-07-17 15:09:05 +0100

    reordering the merged tables to remove problems with duplicates

--- src/d02_intermediate/create_seg_view.py
+++ src/d02_intermediate/create_seg_view.py
@@ -40,25 +40,13 @@ def create_seg_view():
     a_measgraphic_df = io_clean.get_table('a_measgraphic')
     measurement_abstract_rpt_df = io_clean.get_table('measurement_abstract_rpt')
     
-    # 2. Create df by merging a_modvolume, a_measgraphref, a_measgraphic 
-    #        based on 'instanceidk' and 'indexinmglist'
-    df_0 = pd.merge(a_modvolume_df, a_measgraphref_df, how='left'
-                  , on=['instanceidk','indexinmglist'])
-    df_1 = pd.merge(df_0, a_measgraphic_df, how='left'
-                  , on=['instanceidk','indexinmglist'])
-    
-    #3. Add measurement_abstract_rpt to df by merging on 'studyidk' 
-    #        and 'measabstractnumber'
-    df_2 = pd.merge(df_1, measurement_abstract_rpt_df, how='left'
-                  , on=['studyidk','measabstractnumber'])
-    
-    #4. Drop instance id 1789286 and 3121715 due to problems linking and 
-    #        conflicts
-    #   Drop unnessecary row_id from orginal tables (causes problems in dbWrite)
-    df_3 = df_2.drop(df_2[(df_2.instanceidk == 1789286)|(df_2.instanceidk == 3121715)].index)
-    df = df_3.drop(['row_id_x', 'row_id_y', 'row_id_x', 'row_id_y'], 1)
-    
-    #5. Create one hot encoded columns for measurements of interest based
+    # 2. merge a_measgraphref_df, measurement_abstract_rpt_df
+    df = pd.merge(a_measgraphref_df,measurement_abstract_rpt_df, how='left'
+              , on=['studyidk','measabstractnumber'])
+    del a_measgraphref_df
+    del measurement_abstract_rpt_df
+  
+    #3. Create one hot encoded columns for measurements of interest based
     #        on measurement names
     df['AVItd ap4'] = pd.np.where(df.name.str.contains("AVItd ap4"), True, False)
     df['DVItd ap4'] = pd.np.where(df.name.str.contains('DVItd ap4'), True, False)
@@ -83,7 +71,7 @@ def create_seg_view():
     df['Vol. AI (MOD-sp4)'] = pd.np.where(df.name.str.contains("Vol. AI \(MOD-sp4\)"), True, False)
     df['Vol. AI (MOD-sp2)'] = pd.np.where(df.name.str.contains('Vol. AI \(MOD-sp2\)'), True, False)
         
-    #6. Create one shot encode columns for segment names based on one 
+    #4. Create one shot encode columns for segment names based on one 
     #        hot encoded columns for measurements
     df['A4C_Ven_ED'] = (np.where(((df['AVItd ap4'] == True)| 
             (df['DVItd ap4'] == True)|
@@ -107,13 +95,45 @@ def create_seg_view():
     
     df['A4C_Atr_ES'] = np.where((df['Vol. AI (MOD-sp4)'] == True), True, False)
     df['A2C_Atr_ES'] = np.where((df['Vol. AI (MOD-sp2)'] == True), True, False)
-    
-    # 7. Drop one hot encoded columns for measurements of interest
+
+    # 5. Drop one hot encoded columns for measurements of interest
     df.drop(['AVItd ap4', 'DVItd ap4', 'VTD(el-ps4)', 'VTD(MDD-ps4)'
              , 'AVIts ap4', 'DVIts ap4', 'VTS(el-ps4)', 'VTS(MDD-ps4)'
              , 'AVItd ap2', 'DVItd ap2', 'VTD(el-ps2)', 'VTD(MDD-ps2)'
              , 'AVIts ap2', 'DVIts ap2', 'VTS(el-ps2)', 'VTS(MDD-ps2)'
              , 'Vol. AI (MOD-sp4)', 'Vol. AI (MOD-sp2)'], axis=1 , inplace=True)
     
+    # 6. Cut the dataframe to relevant columns as drop duplicates
+    df_1 = df[['studyidk', 'instanceidk', 'indexinmglist', 'A4C_Ven_ED', 'A4C_Ven_ES', 'A2C_Ven_ED', 'A2C_Ven_ES'
+           , 'A4C_Atr_ES', 'A2C_Atr_ES']].copy()
+    del df
+    df_2 = df_1.drop_duplicates()
+    del df_1
+    
+    #  7. merge with a_measgraphic_df for frame number
+    df_3 = pd.merge(df_2, a_measgraphic_df, how='left'
+              , on=['instanceidk','indexinmglist'])
+    del df_2
+    del a_measgraphic_df
+
+    # 8. Cut the dataframe to relevant columns as drop duplicates
+    df_4 = df_3[['studyidk', 'instanceidk', 'indexinmglist', 'A4C_Ven_ED'
+                 , 'A4C_Ven_ES', 'A2C_Ven_ED', 'A2C_Ven_ES', 'A4C_Atr_ES'
+                 , 'A2C_Atr_ES', 'frame']].copy()
+    del df_3
+
+    
+    # 8. merge with a_modvolume
+    df_5 = pd.merge(a_modvolume_df, df_4, how='left'
+                  , on=['instanceidk','indexinmglist'])
+    del df_4
+    del a_modvolume_df
+
+    #4. Drop instance id 1789286 and 3121715 due to problems linking and 
+    #        conflicts
+    #   Drop unnessecary row_id from orginal tables (causes problems in dbWrite)
+    df_6 = df_5.drop(df_5[(df_5.instanceidk == 1789286)|(df_5.instanceidk == 3121715)].index)
+    del df_5
+    
     # write output to db
-    io_views.save_to_db(df, 'frames_by_volume_mask') 
+    io_views.save_to_db(df_6, 'frames_by_volume_mask') 
