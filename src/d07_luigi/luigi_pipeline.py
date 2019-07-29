2f4e7e1b45a0a57c65418fc129dc7b1dcebed210
silil <liliana.millan@gmail.com>
2019-07-30 00:22:27 +0100

    refactoring

--- src/d07_luigi/luigi_pipeline.py
+++ src/d07_luigi/luigi_pipeline.py
@@ -1,11 +1,13 @@
 import luigi
+
 from luigi.contrib.s3 import S3Target
 from luigi.contrib.postgres import PostgresTarget 
 from luigi import LocalTarget
 
+from d00_utils.db_utils import dbReadWriteData
 from d01_data.ingestion_dcm import ingest_dcm
 from d01_data.ingestion_xtdb import ingest_xtdb
-from d02_intermediate.clean_dcm import get_meta_lite
+from d02_intermediate.clean_xtdb import clean_tables
 
 
 def get_postgres_credentials():
@@ -21,8 +23,8 @@ def get_postgres_credentials():
 class S3Bucket(luigi.ExternalTask): # ensure connection to S3 bucket
 
     def output(self):
-	return luigi.S3Target('s3://cibercv/')
-	# e.g. https://stackoverflow.com/questions/33332058/luigi-pipeline-beginning-in-s3
+        return luigi.S3Target('s3://cibercv/')
+    # e.g. https://stackoverflow.com/questions/33332058/luigi-pipeline-beginning-in-s3
 
 
 class IngestDCM(luigi.Task):
@@ -31,13 +33,13 @@ class IngestDCM(luigi.Task):
     Populates postgres db schema raw
     '''
     def requires(self):
-	return S3Bucket()
+        return S3Bucket()
 
     def output(self):
         host, user, psswd, database = get_postgres_credentials()
         table = 'raw.metadata'
         update_id = 'ingest'
-        return PostgresTarget(host, database, user, password, table, update_id)
+        return PostgresTarget(host, database, user, psswd, table, update_id)
 	
     def run(self):
         ingest_dcm()
@@ -56,7 +58,7 @@ class CleanDCM(luigi.Task):
         table = 'clean.meta_lite'
         update_id = 'clean'
         return PostgresTarget(host, database, user, password, table, update_id)
-    	# https://luigi.readthedocs.io/en/stable/api/luigi.contrib.postgres.html
+        # https://luigi.readthedocs.io/en/stable/api/luigi.contrib.postgres.html
         # e.g. https://vsupalov.com/luigi-query-postgresql/
 
 
@@ -74,7 +76,8 @@ class IngestXTDB(luigi.Task):
         host, user, password, database = get_postgres_credentials()
         table = 'raw.A_measgraphic'
         update_id = 'ingest'
-	return PostgresTarget(host, database, user, password, table, update_id)
+
+        return PostgresTarget(host, database, user, password, table, update_id)
 
     def run(self):
         return ingest_xtdb()
@@ -88,16 +91,17 @@ class CleanXTDB(luigi.Task):
     This assumption is because clean_tables() cleans all tables at once
     '''
     def requires (self):
-	return IngestXTDB()
+        return IngestXTDB()
 
     def output(self): 
-	host, user, password, database = get_postgres_credentials()
+        host, user, password, database = get_postgres_credentials()
         table = 'clean.a_measgraphic'
         update_id = 'clean'
-	return PostgresTarget(host, database, user, password, table, update_id)
+        return PostgresTarget(host, database, user, password, table, update_id)
 
     def run(self):
         return clean_tables()
+
 
 class Pipeline(luigi.WrapperTask):
     '''
