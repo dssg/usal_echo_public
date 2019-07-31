1767bbf3e8a1363669c64bdc75fbb28599f54885
courtneyjean84 <courtney_irwin@hotmail.com>
2019-07-31 16:22:36 +0100

    updated with yoni's comments

--- src/d04_segmentation/create_probabilities_textfile_testing.py
+++ src/d04_segmentation/create_probabilities_textfile_testing.py
@@ -10,15 +10,17 @@ import os
 import datetime
 import pandas as pd
 
-def create_probabilitises_textfile_testing(instance_id_list):
+def create_probabilities_textfile_testing(instance_id_list):
     """
+    Produces text file with view ground truths for segmentation model testing. 
+    
     This function writes a text file in the same format as the output of the 
     view classification model, but replacing the model classification 
     probabilities with the ground truth classification labels.
     
     
     The following steps are performend:
-        1. Gets frames_by_volume_mask view & instances_unique_master_list
+        1. Gets io_views: frames_by_volume_mask view & instances_unique_master_list
         2. Merges these to get filenames
         3. Filters the resulting table by param: instanceids
         4. Create probabilities table
@@ -28,20 +30,20 @@ def create_probabilitises_textfile_testing(instance_id_list):
     :return nothing
     """
     
-    #1. Gets frames_by_volume_mask view
+    # 1. Gets frames_by_volume_mask view
     io_views = dbReadWriteViews()
     frames_by_volume_mask = io_views.get_table('frames_by_volume_mask')
     instances_unique_master_list = io_views.get_table('instances_unique_master_list')
     
-    #2. Merges tables
+    # 2. Merges tables
     df_1 = pd.merge(frames_by_volume_mask, instances_unique_master_list
                   , how='left', on =['instanceidk', 'studyidk'])
     
-    #3. Filters the dataframe by the param: instanceid
+    # 3. Filters the dataframe by the param: instanceid
     df_2 = df_1[df_1['instanceidk'].isin(instance_id_list)]
     del df_1
     
-    #4. Create the probabilities table
+    # 4. Create the probabilities table
     prob_tb = pd.DataFrame(columns=["study", "image", "plax_far", "plax_plax"
                                  , "plax_laz", "psax_az", "psax_mv"
                                  , "psax_pap", "a2c_lvocc_s", "a2c_laocc"
@@ -51,8 +53,9 @@ def create_probabilitises_textfile_testing(instance_id_list):
                                  , "suprasternal", "subcostal", "plax_lac"
                                  , "psax_apex"])
     
+    prob_tb.at[:, "study"] = '/home/ubuntu/data/01_raw/dcm_sample_labelled'
+    
     for i in df_2.index:
-        prob_tb.at[i, "study"] = '/home/ubuntu/data/01_raw/dcm_sample_labelled'
         prob_tb.at[i, "image"] = df_2.at[i, 'instancefilename']
         if df_2.at[i, 'view_only'] == 'a4c':
             prob_tb.at[i, "a4c"] = 1
@@ -61,7 +64,7 @@ def create_probabilitises_textfile_testing(instance_id_list):
     
     df_3 = prob_tb.fillna(0)
     
-    #5. Writes the probabilities textfile to data/d04_segmentation folder
+    # 5. Writes the probabilities textfile to data/d04_segmentation folder
     project_dir = os.path.dirname(os.getcwd())
     data_dir = os.path.join(project_dir, "data", "d04_segmentation")
     
@@ -72,6 +75,6 @@ def create_probabilitises_textfile_testing(instance_id_list):
     file_name = 'view_probabilities_test' + str(time_stamp)
     data_path = os.path.join(data_dir, file_name)
         
-    df_3.to_csv (data_path, index = None, header=True) 
+    df_3.to_csv(data_path, index = None, header=True) 
     
     
