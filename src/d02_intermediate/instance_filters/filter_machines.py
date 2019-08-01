27441734f682cd30c60ff75d24fa591f19d10236
davevanveen <davemvanveen@gmail.com>
2019-08-01 14:47:53 +0000

    added hierarchy for sub scripts

---
+++ src/d02_intermediate/instance_filters/filter_machines.py
@@ -0,0 +1,41 @@
+import pandas
+import sys
+
+from d00_utils.db_utils import dbReadWriteClean, dbReadWriteViews
+
+"""
+This script contains filter_by_machine_type(), which creates
+the tables views.machines_all_bmi and views.machines_new_bmi,
+which contain the following columns: studyidk, equipment, bmi
+
+machines_all_bmi contains machine type + bmi for every study in db
+machines_new_bmi contains machine type + bmi for studies taken on
+    "new" machines, i.e. ECOEPIQ2, EPIQ7-1, ECOIE33, AFFINITI_1, AFFINITI_2
+"""
+
+
+def filter_by_machine_type():
+    io_clean = dbReadWriteClean()
+    io_views = dbReadWriteViews()
+
+    # get relevant table, drop unnecessary columns
+    summ_df = io_clean.get_table("dm_spain_view_study_summary")
+    cols_to_drop = [
+        "age",
+        "gender",
+        "patientweight",
+        "patientheight",
+        "findingcode",
+        "findingcodetext",
+        "conclusion",
+        "reasonforstudy",
+        "studylocation",
+        "modality",
+    ]
+    machine_df = summ_df.drop(labels=cols_to_drop, axis=1)
+    io_views.save_to_db(machine_df, "machines_all_bmi")
+
+    # filter out old machines
+    machines_to_keep = ["ECOEPIQ2", "EPIQ7-1", "ECOIE33", "AFFINITI_1", "AFFINITI_2"]
+    machine_new_df = machine_df[machine_df["equipment"].isin(machines_to_keep)]
+    io_views.save_to_db(machine_new_df, "machines_new_bmi")
