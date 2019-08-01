ee1f3f97a590d093aeed1d4ba32b3f04b8a266dd
davevanveen <davemvanveen@gmail.com>
2019-08-01 16:40:51 +0000

    finalized view filtering by csvs

--- src/d02_intermediate/create_dataset.py
+++ src/d02_intermediate/create_dataset.py
@@ -1,11 +1,62 @@
 import sys
-sys.path.append('instance_filters/')
 
-import filter_machines
+from d02_intermediate.instance_filters.filter_machines import filter_by_machine_type
+from d02_intermediate.instance_filters.master_list import create_master_instance_list
+from d02_intermediate.instance_filters.filter_views import filter_by_views
+from d02_intermediate.instance_filters.conflict_instances import find_table_conflicts
+from d02_intermediate.instance_filters.unlabeled_instances import create_unlabeled_instances
+from d02_intermediate.instance_filters.train_test_split import split_train_test
+from d02_intermediate.instance_filters.downsample_data import downsample_train_test
+
 
 '''
 This is a high-level script which runs all filtering of instances needed to create the dataset.
 Summaries of each filtering steps are included below. Further details can be found in the corresponding script
 '''
+def run_all():
 
-filter_machines.filter_by_machine_type()
+    ''' 
+    Creates two tables:
+		views.machines_all_bmi: list of all studies in db; columns: studyidk, machine type and bmi
+		views.machines_new_bmi: same as machines_all_bmi, but only includes studies with new machines, i.e. machine types ECOEPIQ2, EPIQ7-1, ECOIE33, AFFINITI_1, AFFINITI_2
+    '''
+	filter_by_machine_type()
+
+
+	'''
+	Creates views.instances_unique_master_list, a list of unique instances in the database. Unique means that instances with naming conflicts (e.g. duplicate instanceidk's) have been removed
+	'''
+	create_master_instance_list()
+
+
+	'''
+	Creates many tables:
+		views.frames_with_labels: all frames with labels plax, a4c, a2c
+		views.instances_with_labels: all instances which are labeled plax, a4c, a2c
+			Assumption: if a frame has a view label, other frames within that instance correspond to the same view. This discludes instances which has >1 frames with conflicting labels
+		views.frames_sorted_by_views_temp: intermediate table; used by other scripts
+	'''
+    filter_by_views()
+
+
+	'''
+	Creates views.instances_with_conflicts, i.e. instances to avoid
+	'''
+	find_table_conflicts()
+
+	'''
+	Creates views.instances_labeled_other: all instances which are fair game for classifying as others, i.e. instances which are from studies that contain labels + no conflicts. The true views of these instances are not known; we only know that they haven't been explicitly labeled plax, a4c, a2c
+	'''
+	create_unlabeled_instances()
+
+
+	'''
+	Creates views.instances_w_labels_{train/test} from views.instances_with_labels based on a hard-coded split ratio
+	'''
+	split_train_test()
+
+	'''
+	Creates views.instances_w_labels_{train/test}_downsampX, i.e. versions of views.instances_w_labels_{train/test} that have been downsampled by a factor of X
+	'''
+	downsample_train_test()
+	
