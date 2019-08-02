d726e63a84af74c53bdfac14fc42b435af2aaea3
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-02 11:14:33 +0100

    fixed errors on create prob testfile script

--- src/d04_segmentation/create_probabilities_textfile_testing.py
+++ src/d04_segmentation/create_probabilities_textfile_testing.py
@@ -53,8 +53,6 @@ def create_probabilities_textfile_testing(instance_id_list):
                                  , "suprasternal", "subcostal", "plax_lac"
                                  , "psax_apex"])
     
-    prob_tb.at[:, "study"] = '/home/ubuntu/data/01_raw/dcm_sample_labelled'
-    
     for i in df_2.index:
         instancefilename = df_2.at[i, 'instancefilename']
         studyidk = df_2.at[i, 'studyidk']
@@ -66,6 +64,7 @@ def create_probabilities_textfile_testing(instance_id_list):
             prob_tb.at[i, "a2c"] = 1 
     
     df_3 = prob_tb.fillna(0)
+    df_3 = df_3.at[:, "study"] = '/home/ubuntu/data/01_raw/dcm_sample_labelled'
     
     # 5. Writes the probabilities textfile to data/d04_segmentation folder
     project_dir = os.path.dirname(os.getcwd())
