477c30afbd83687595865e6970c456a500bf513c
Yoni Nachmany <yoninachmany@gmail.com>
2019-08-05 14:19:10 +0000

    reorder fxn

--- src/d04_segmentation/analyse_segments.py
+++ src/d04_segmentation/analyse_segments.py
@@ -8,152 +8,15 @@ from d00_utils.dcm_utils import *
 from d00_utils.output_utils import *
 
 
-def point_distance(point1, point2):
-    point1 = np.array(point1).astype(float)
-    point2 = np.array(point2).astype(float)
-    return np.sqrt(np.sum((point1 - point2) ** 2))
-
-
-def L(seg, x, y, x_la, y_la):
-    fit = np.polyfit(x, y, 1)
-    fit_fn = np.poly1d(fit)
-    line_points = fit_fn(x_la)
-    x_min = x_la[np.argmin(line_points)]
-    y_min = np.min(line_points)
-    x_max = x_la[np.argmax(line_points)]
-    y_max = np.max(line_points)
-    l = point_distance([x_min, y_min], [x_max, y_max])
-    return l
-
-
-def extract_area_l_scaled(
-    video,
-    directory,
-    lv_segs,
-    la_segs,
-    la_areas,
-    lv_areas,
-    x_scale,
-    y_scale,
-    rows,
-    cols,
-    hr,
-):
-    # left atrium analysis
-    la_seg = la_segs[np.argsort(la_areas)[int(0.80 * len(la_segs))]]
-    lv_seg = lv_segs[np.argsort(la_areas)[int(0.80 * len(la_segs))]]
-    seg = lv_seg
-    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
-    la_seg = imresize(la_seg.copy(), (rows, cols), interp="nearest")
-    x, y = np.where(seg > 0)
-    x_la, y_la = np.where(la_seg > 0)
-    l_la = L(la_seg, x, y, x_la, y_la)
-
-    # Within window, 90% and 10% of areas as LV end-diastolic/systolic areas.
-    lveda_seg = lv_segs[np.argsort(lv_areas)[int(0.90 * len(lv_segs))]]
-    lvesa_seg = lv_segs[np.argsort(lv_areas)[int(0.10 * len(lv_segs))]]
-    la_seg = la_segs[np.argsort(la_areas)[int(0.90 * len(lv_segs))]]
-
-    # left ventricular diastolic volume analysis
-    seg = la_seg.copy()
-    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
-    lveda_seg = imresize(lveda_seg.copy(), (rows, cols), interp="nearest")
-    x, y = np.where(seg > 0)
-    x_lveda, y_lveda = np.where(lveda_seg > 0)
-    l_lveda = L(lveda_seg, x, y, x_lveda, y_lveda)
-
-    # left ventricular systolic volume analysis
-    seg = la_seg.copy()
-    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
-    lvesa_seg = imresize(lvesa_seg.copy(), (rows, cols), interp="nearest")
-    x, y = np.where(seg > 0)
-    x_lvesa, y_lvesa = np.where(lvesa_seg > 0)
-    l_lvesa = L(lvesa_seg, x, y, x_lvesa, y_lvesa)
-
-    return (
-        len(x_la) * x_scale ** 2,
-        l_la * x_scale,
-        len(x_lveda) * x_scale ** 2,
-        l_lveda * x_scale,
-        len(x_lvesa) * x_scale ** 2,
-        l_lvesa * x_scale,
-        hr,
-    )
-
-
-def compute_volume_AL(area, length):
-    volume = 0.85 * area ** 2 / length
-    return volume
-
-
-def compute_diastole(lv_areas_window, ft):
-    windowlength = len(lv_areas_window)
-    minarea = np.min(lv_areas_window[: int(0.6 * windowlength)])
-    maxarea = np.max(lv_areas_window)
-    minindex = lv_areas_window.index(minarea)
-    maxindex = lv_areas_window.index(minarea)
-    half = 0.5 * (minarea + maxarea)
-    halfarea = [i for i in lv_areas_window[maxindex:] if i > half]
-    if not halfarea == []:
-        halfareachoice = halfarea[0]
-        halfindex = lv_areas_window.index(halfareachoice)
-        diasttime = ft * (halfindex - minindex)
-        return diasttime
-    else:
-        return np.nan
-
-
-def apply_rolling_window(areas):
-    areas = (
-        pd.DataFrame(areas)[0]
-        .rolling(window=4, center=True)
-        .median()
-        .fillna(method="bfill")
-        .fillna(method="ffill")
-        .tolist()
-    )
-    return areas
-
-
-def extract_areas(segs):
-    areas = []
-    for seg in segs:
-        area = len(np.where(seg > 0)[0])
-        areas.append(area)
-    return areas
-
-
-def remove_periphery(imgs):
+def get_window(hr, ft):
     """
+    Estimate duration of cardiac cycle with heart rate and frame time.
+
+    (seconds/beat) / (seconds/frame) = frames/beat
 
     """
-    imgs_ret = []
-    for img in imgs:
-        image = img.astype("uint8").copy()
-        fullsize = image.shape[0] * image.shape[1]
-        image[image > 0] = 255
-        image = cv2.bilateralFilter(image, 11, 17, 17)
-        thresh = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)[1]
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        contours = cnts[1]
-        areas = []
-        for i in range(0, len(contours)):
-            areas.append(cv2.contourArea(contours[i]))
-
-        if len(areas) == 0:
-            imgs_ret.append(img)
-        else:
-            select = np.argmax(areas)
-            roi_corners_clean = []
-            roi_corners = np.array(contours[select], dtype=np.int32)
-            for i in roi_corners:
-                roi_corners_clean.append(i[0])
-            hull = cv2.convexHull(np.array([roi_corners_clean], dtype=np.int32))
-            mask = np.zeros(image.shape, dtype=np.uint8)
-            mask = cv2.fillConvexPoly(mask, hull, 1)
-            imgs_ret.append(img * mask)
-    return np.array(imgs_ret)
+    window = int(((60 / hr) / (ft / 1000)))
+    return window
 
 
 def compute_la_lv_volume(
@@ -257,15 +120,152 @@ def compute_la_lv_volume(
     }
 
 
-def get_window(hr, ft):
+def remove_periphery(imgs):
     """
-    Estimate duration of cardiac cycle with heart rate and frame time.
-
-    (seconds/beat) / (seconds/frame) = frames/beat
 
     """
-    window = int(((60 / hr) / (ft / 1000)))
-    return window
+    imgs_ret = []
+    for img in imgs:
+        image = img.astype("uint8").copy()
+        fullsize = image.shape[0] * image.shape[1]
+        image[image > 0] = 255
+        image = cv2.bilateralFilter(image, 11, 17, 17)
+        thresh = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        contours = cnts[1]
+        areas = []
+        for i in range(0, len(contours)):
+            areas.append(cv2.contourArea(contours[i]))
+
+        if len(areas) == 0:
+            imgs_ret.append(img)
+        else:
+            select = np.argmax(areas)
+            roi_corners_clean = []
+            roi_corners = np.array(contours[select], dtype=np.int32)
+            for i in roi_corners:
+                roi_corners_clean.append(i[0])
+            hull = cv2.convexHull(np.array([roi_corners_clean], dtype=np.int32))
+            mask = np.zeros(image.shape, dtype=np.uint8)
+            mask = cv2.fillConvexPoly(mask, hull, 1)
+            imgs_ret.append(img * mask)
+    return np.array(imgs_ret)
+
+
+def extract_areas(segs):
+    areas = []
+    for seg in segs:
+        area = len(np.where(seg > 0)[0])
+        areas.append(area)
+    return areas
+
+
+def apply_rolling_window(areas):
+    areas = (
+        pd.DataFrame(areas)[0]
+        .rolling(window=4, center=True)
+        .median()
+        .fillna(method="bfill")
+        .fillna(method="ffill")
+        .tolist()
+    )
+    return areas
+
+
+def extract_area_l_scaled(
+    video,
+    directory,
+    lv_segs,
+    la_segs,
+    la_areas,
+    lv_areas,
+    x_scale,
+    y_scale,
+    rows,
+    cols,
+    hr,
+):
+    # left atrium analysis
+    la_seg = la_segs[np.argsort(la_areas)[int(0.80 * len(la_segs))]]
+    lv_seg = lv_segs[np.argsort(la_areas)[int(0.80 * len(la_segs))]]
+    seg = lv_seg
+    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
+    la_seg = imresize(la_seg.copy(), (rows, cols), interp="nearest")
+    x, y = np.where(seg > 0)
+    x_la, y_la = np.where(la_seg > 0)
+    l_la = L(la_seg, x, y, x_la, y_la)
+
+    # Within window, 90% and 10% of areas as LV end-diastolic/systolic areas.
+    lveda_seg = lv_segs[np.argsort(lv_areas)[int(0.90 * len(lv_segs))]]
+    lvesa_seg = lv_segs[np.argsort(lv_areas)[int(0.10 * len(lv_segs))]]
+    la_seg = la_segs[np.argsort(la_areas)[int(0.90 * len(lv_segs))]]
+
+    # left ventricular diastolic volume analysis
+    seg = la_seg.copy()
+    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
+    lveda_seg = imresize(lveda_seg.copy(), (rows, cols), interp="nearest")
+    x, y = np.where(seg > 0)
+    x_lveda, y_lveda = np.where(lveda_seg > 0)
+    l_lveda = L(lveda_seg, x, y, x_lveda, y_lveda)
+
+    # left ventricular systolic volume analysis
+    seg = la_seg.copy()
+    seg = imresize(seg.copy(), (rows, cols), interp="nearest")
+    lvesa_seg = imresize(lvesa_seg.copy(), (rows, cols), interp="nearest")
+    x, y = np.where(seg > 0)
+    x_lvesa, y_lvesa = np.where(lvesa_seg > 0)
+    l_lvesa = L(lvesa_seg, x, y, x_lvesa, y_lvesa)
+
+    return (
+        len(x_la) * x_scale ** 2,
+        l_la * x_scale,
+        len(x_lveda) * x_scale ** 2,
+        l_lveda * x_scale,
+        len(x_lvesa) * x_scale ** 2,
+        l_lvesa * x_scale,
+        hr,
+    )
+
+
+def L(seg, x, y, x_la, y_la):
+    fit = np.polyfit(x, y, 1)
+    fit_fn = np.poly1d(fit)
+    line_points = fit_fn(x_la)
+    x_min = x_la[np.argmin(line_points)]
+    y_min = np.min(line_points)
+    x_max = x_la[np.argmax(line_points)]
+    y_max = np.max(line_points)
+    l = point_distance([x_min, y_min], [x_max, y_max])
+    return l
+
+
+def point_distance(point1, point2):
+    point1 = np.array(point1).astype(float)
+    point2 = np.array(point2).astype(float)
+    return np.sqrt(np.sum((point1 - point2) ** 2))
+
+
+def compute_volume_AL(area, length):
+    volume = 0.85 * area ** 2 / length
+    return volume
+
+
+def compute_diastole(lv_areas_window, ft):
+    windowlength = len(lv_areas_window)
+    minarea = np.min(lv_areas_window[: int(0.6 * windowlength)])
+    maxarea = np.max(lv_areas_window)
+    minindex = lv_areas_window.index(minarea)
+    maxindex = lv_areas_window.index(minarea)
+    half = 0.5 * (minarea + maxarea)
+    halfarea = [i for i in lv_areas_window[maxindex:] if i > half]
+    if not halfarea == []:
+        halfareachoice = halfarea[0]
+        halfindex = lv_areas_window.index(halfareachoice)
+        diasttime = ft * (halfindex - minindex)
+        return diasttime
+    else:
+        return np.nan
 
 
 def calculate_measurements():
