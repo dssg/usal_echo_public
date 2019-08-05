a7a6f5b67c4f803f8025b6382cce57123c6f96eb
davevanveen <davemvanveen@gmail.com>
2019-08-05 10:31:22 +0000

    commented train_test_split and downsample. WT will implement downstream

--- src/d02_intermediate/filter_instances.py
+++ src/d02_intermediate/filter_instances.py
@@ -1,12 +1,12 @@
 import sys
 
-from d02_intermediate.instance_filters.filter_machines import filter_by_machine_type
-from d02_intermediate.instance_filters.master_list import create_master_instance_list
-from d02_intermediate.instance_filters.filter_views import filter_by_views
-from d02_intermediate.instance_filters.conflict_instances import find_table_conflicts
-from d02_intermediate.instance_filters.unlabeled_instances import create_unlabeled_instances
-from d02_intermediate.instance_filters.train_test_split import split_train_test
-from d02_intermediate.instance_filters.downsample_data import downsample_train_test
+from d02_intermediate.instance_filters import filter_machines
+from d02_intermediate.instance_filters import master_list
+from d02_intermediate.instance_filters import filter_views
+from d02_intermediate.instance_filters import conflict_instances
+from d02_intermediate.instance_filters import unlabeled_instances
+from d02_intermediate.instance_filters import train_test_split
+from d02_intermediate.instance_filters import downsample_data
 
 
 '''
@@ -21,14 +21,14 @@ def run_all():
 		views.machines_new_bmi: same as machines_all_bmi, but only includes studies with new machines, 
                                 i.e. machine types ECOEPIQ2, EPIQ7-1, ECOIE33, AFFINITI_1, AFFINITI_2
     '''
-	filter_by_machine_type()
+	filter_machines.filter_by_machine_type()
 
 
 	'''
 	Creates views.instances_unique_master_list, a list of unique instances in the database. 
     Unique means that instances with naming conflicts (e.g. duplicate instanceidk's) have been removed
 	'''
-	create_master_instance_list()
+	master_list.create_master_instance_list()
 
 
 	'''
@@ -40,13 +40,13 @@ def run_all():
                         conflicting labels
 		views.frames_sorted_by_views_temp: intermediate table; used by other scripts
 	'''
-    filter_by_views()
+    filter_views.filter_by_views()
 
 
 	'''
 	Creates views.instances_w_conflicts, i.e. instances to avoid
 	'''
-	find_table_conflicts()
+	conflict_instances.find_table_conflicts()
 
 	'''
 	Creates views.instances_labeled_other: all instances which are fair game for classifying as 
@@ -54,7 +54,7 @@ def run_all():
     The true views of these instances are not known; we only know that they haven't been 
     explicitly labeled plax, a4c, a2c
 	'''
-	create_unlabeled_instances()
+	unlabeled_instances.create_unlabeled_instances()
 
 
 	'''
@@ -64,7 +64,7 @@ def run_all():
     :param ratio(float): ratio for splitting into train/test
                          e.g. if 0.8, will take 80% as train set and 20% as test set
 	'''
-	split_train_test(ratio=0.5)
+	#train_test_split.split_train_test(ratio=0.5)
 
 	'''
 	Creates views.instances_w_labels_{train/test}_downsampX, i.e. versions of 
@@ -73,5 +73,5 @@ def run_all():
     :param ratio(float): percentage by which to downsample dataset
                          e.g. if ratio=0.1, will downsample by a factor of 10
     '''
-	downsample_train_test(ratio=0.1)
+	#downsample_data.downsample_train_test(ratio=0.1)
 	
