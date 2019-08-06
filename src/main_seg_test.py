8afa2a8cfa27833f734b3e24a15002366d27cdbf
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-06 13:08:44 +0100

    functions to support writing to segmentation schema

--- src/main_seg_test.py
+++ src/main_seg_test.py
@@ -4,7 +4,7 @@ from d02_intermediate import clean_dcm, clean_xtdb
 from d03_classification import predict_view_v0
 from d04_segmentation import segment_view
 
-from d02_intermediate import train_test_split
+#from d02_intermediate import train_test_split
 
 
 if __name__ == "__main__":
