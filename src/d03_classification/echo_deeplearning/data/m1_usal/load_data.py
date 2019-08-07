7331a63b765ba43031d07575c97a80eef655079a
davevanveen <davemvanveen@gmail.com>
2019-08-07 16:45:26 +0000

    finalized v1 of load_data

--- src/d03_classification/echo_deeplearning/data/m1_usal/load_data.py
+++ src/d03_classification/echo_deeplearning/data/m1_usal/load_data.py
@@ -2,37 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
 import random
-
-sys.path.append("../../src")
-from util import * ## e.g. from echocv/utils. do we have this in our repo?
+#from util import * ## e.g. from echocv/utils. do we have this in our repo?
                     # may not need it, as they only import 1 function (I think), i.e. split_data()
 from scipy.misc import imread
 
+sys.path.append('../../src/')
 from d00_utils.db_utils import dbReadWriteViews
+
 
 io_views = dbReadWriteViews()
 df = io_views.get_table('instances_w_labels')
 
+
 def get_onehot_vector(filename_str):
     ''' 
-    Returns a one-hot vector corresponding to 
+    Returns a one-hot vector corresponding to 23-class view classification model
+    @params filename_str: name of .jpg file, e.g. a_113150_2WD5N6AS_53.jpg
     '''
     filename = filename_str.split('_')[2].split('.')[0]
     #filename = '8Z0BWX0M' # TO DO: delete this line!
     df_row = df[df['filename']==filename]
     label = df_row['view'].tolist()[0]
 
-    vec = [0] * 23
+    vec = [0] * 23 # note: hard-coded for 23-class model
     mapping = {'plax': 1, 'a2c': 8, 'a4c': 14}
     idx = mapping[label]
     vec[idx] = 1
     
     return vec
 
-## TO DO:
-# Use this file as a template for getting data in the same format
-# Look at d00_utils/dcm_utils, which has preprocessing scripts improved from Zhang
-# We already split data into train/test. May not need to process like this
 
 def load_data(config, val_split): ## may be able
     """
@@ -49,8 +47,7 @@ def load_data(config, val_split): ## may be able
 
     @params config: dictionary of hyperparemeters
     """
-
-    # config.data = 'm1_usal', config.label_dim = 2, config.feature_dim = 1, config.image_size=224
+    # config.data = 'm1_usal', config.label_dim = 23, config.feature_dim = 1, config.image_size=224
     
     # TO DO: soft code this later, make sure it's correct with our actual data location
     directory = '~/data/02_intermediate/test'
@@ -58,47 +55,35 @@ def load_data(config, val_split): ## may be able
     directory_test = '~/data/02_intermediate/test_downsampleby5'  
     directories = [directory_train, directory_test]
 
-    #data_index = [0, 1]
-    img_train = []
-    img_test = []
-    labels = []
-    filenames = []
+    img_train, img_test, label_train, label_test, filenames = ([] for i in range(5))
 
-    # WHAT I WANT: two separate numpy arrays x_train, x_test
-    # Getting them from separate directories
+    # Getting x_train, x_test from separate directories
     # Note: they import util from echocv/util.py
-    # Note: to get y_train, y_test, will need to use study/instance and look up value in table
-    #       first figure out how we want to load files. stripping strings? meh
 
     for directory in directories:
         for filename in os.listdir(directory):
             if filename not in filenames:
                 filenames.append(filename)
+                label_vec = get_onehot_vec(filename)
                 if directory == directory_train:
                     img_train.append(np.load(directory + filename).astype("uint8")[:, :, :])
+                    label_train.append(label_vec)
                 elif directory == directory_test:
                     img_test.append(np.load(directory + filename).astype("uint8")[:, :, :])
-                #label = [0] * config.label_dim
-                #label[data_index[i]] = 1
-                #labels.append(label)
+                    label_test.append(label_vec)
     
     x_train = np.array(img_train).reshape(
         (len(x_train), config.image_size, config.image_size, config.feature_dim)
     )
-
     x_test = np.array(img_test).reshape(
         (len(x_test), config.image_size, config.image_size, config.feature_dim)
     )
-
-       
-    # what do we actually want to load in as our y? Currently we have strings in a db
-    # they used config.label_dim to get dim two, i.e. case vs control
-    # I assume we want a four (or 23) dim vector one-hot encoded by view
-    y_train = np.array(y_train).reshape((len(y_train), config.label_dim))
-    y_test = np.array(y_test).reshape((len(y_test), config.label_dim))
+    y_train = np.array(label_train).reshape((len(y_train), config.label_dim))
+    y_test = np.array(label_test).reshape((len(y_test), config.label_dim))
+    
     return x_train, x_test, y_train, y_test
 
-
+'''
 ## ALL THIS DOES is make a list of all the filenames in the directory. why do we need this?
 ## (note: redundant with code above)
 def load_filenames():
@@ -114,3 +99,4 @@ def load_filenames():
                 filenames.append(filename)
 
     return filenames
+'''
