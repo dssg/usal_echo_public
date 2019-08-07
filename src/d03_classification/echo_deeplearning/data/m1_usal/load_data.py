7a2cbbbf5294cc687070905c5fcac3895fa7f602
davevanveen <davemvanveen@gmail.com>
2019-08-07 12:57:08 +0000

    reconfigured script to our setting, left comments with TODO's

--- src/d03_classification/echo_deeplearning/data/m1_usal/load_data.py
+++ src/d03_classification/echo_deeplearning/data/m1_usal/load_data.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import os, sys
 import random
 
-sys.path.append("src/")
-from util import * ## e.g. from out utils. match up functions
+sys.path.append("../../src")
+from util import * ## e.g. from echocv/utils. do we have this in our repo?
+                    # may not need it, as they only import 1 function (I think), i.e. split_data()
 from scipy.misc import imread
 
 ## TO DO:
@@ -12,7 +13,7 @@ from scipy.misc import imread
 # Look at d00_utils/dcm_utils, which has preprocessing scripts improved from Zhang
 # We already split data into train/test. May not need to process like this
 
-def load_data(config, val_split):
+def load_data(config, val_split): ## may be able
     """
     Preprocesses data and loads them into numpy arrays
     Returns
@@ -26,59 +27,59 @@ def load_data(config, val_split):
         label_dim is number of unique labels
 
     @params config: dictionary of hyperparemeters
-    @val_split: split data into training and test based on validation split
     """
-    #######################################################################
-    # REPLACE WITH YOUR OWN PREPROCESSING METHODS THAT RETURN SAME FORMAT #
-    #######################################################################
 
     # config.data = 'm1_usal', config.label_dim = 2, config.feature_dim = 1, config.image_size=224
     
-    # Get one directory of images; Question: are these direcs below their preprocessed data?
-    # i.e. do we need to run pre-processing on ours first, or can we download + use directly from db
-    directories = [
-        "/media/deoraid03/deeplearning/hcm_case_control_pklfiles/a4c/ctl/systdiastpairs/",
-        "/media/deoraid03/deeplearning/hcm_case_control_pklfiles/a4c/case/systdiastpairs/",
-    ]
-    data_index = [0, 1]
-    images = []
+    # TO DO: soft code this later, make sure it's correct with our actual data location
+    directory = '~/data/02_intermediate/test'
+    directory_train = '~/data/02_intermediate/train_downsampleby5'
+    directory_test = '~/data/02_intermediate/test_downsampleby5'  
+    directories = [directory_train, directory_test]
+
+    #data_index = [0, 1]
+    img_train = []
+    img_test = []
     labels = []
     filenames = []
 
-    # Delete outer for loop assuming one directory
-    for i, directory in enumerate(directories):
+    # WHAT I WANT: two separate numpy arrays x_train, x_test
+    # Getting them from separate directories
+    # Note: they import util from echocv/util.py
+    # Note: to get y_train, y_test, will need to use study/instance and look up value in table
+    #       first figure out how we want to load files. stripping strings? meh
+
+    for directory in directories:
         for filename in os.listdir(directory):
             if filename not in filenames:
-                images.append(np.load(directory + filename).astype("uint8")[:, :, :])
                 filenames.append(filename)
-                label = [0] * config.label_dim
-                label[data_index[i]] = 1
-                labels.append(label)
-    images = np.array(images)
-    labels = np.array(labels)
+                if directory == directory_train:
+                    img_train.append(np.load(directory + filename).astype("uint8")[:, :, :])
+                elif directory == directory_test:
+                    img_test.append(np.load(directory + filename).astype("uint8")[:, :, :])
+                #label = [0] * config.label_dim
+                #label[data_index[i]] = 1
+                #labels.append(label)
     
-    train_lst = np.load(
-        "data/" + config.data + "/splits/train_lst_" + str(val_split) + ".npy"
-    )
-    val_lst = np.load(
-        "data/" + config.data + "/splits/val_lst_" + str(val_split) + ".npy"
-    )
-
-    x_train, y_train, x_test, y_test = split_data(
-        filenames, images, labels, train_lst, val_lst
-    )
-
-    x_train = np.array(x_train).reshape(
+    x_train = np.array(img_train).reshape(
         (len(x_train), config.image_size, config.image_size, config.feature_dim)
     )
-    y_train = np.array(y_train).reshape((len(y_train), config.label_dim))
-    x_test = np.array(x_test).reshape(
+
+    x_test = np.array(img_test).reshape(
         (len(x_test), config.image_size, config.image_size, config.feature_dim)
     )
+
+       
+    # what do we actually want to load in as our y? Currently we have strings in a db
+    # they used config.label_dim to get dim two, i.e. case vs control
+    # I assume we want a four (or 23) dim vector one-hot encoded by view
+    y_train = np.array(y_train).reshape((len(y_train), config.label_dim))
     y_test = np.array(y_test).reshape((len(y_test), config.label_dim))
     return x_train, x_test, y_train, y_test
 
 
+## ALL THIS DOES is make a list of all the filenames in the directory. why do we need this?
+## (note: redundant with code above)
 def load_filenames():
     directories = [
         "/media/deoraid03/deeplearning/hcm_case_control_pklfiles/a4c/ctl/systdiastpairs/",
