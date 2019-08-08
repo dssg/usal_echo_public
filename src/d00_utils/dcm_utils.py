d5ad8465fd5c8f50ff9dac11751a54f4ed849c3f
Yoni Nachmany <yoninachmany@gmail.com>
2019-08-08 13:10:02 +0000

    black

--- src/d00_utils/dcm_utils.py
+++ src/d00_utils/dcm_utils.py
@@ -234,9 +234,15 @@ def extract_metadata_for_measurements(dicomdir, videofile):
     text = pipe.communicate()[0]
     lines = text.split("\n")
     dicom_tags = json.load(open("d02_intermediate/dicom_tags.json"))
-    dicom_tags = {k: str(tuple(v)).replace("'", "").replace(" ", "") for k,v in dicom_tags.items()}
+    dicom_tags = {
+        k: str(tuple(v)).replace("'", "").replace(" ", "")
+        for k, v in dicom_tags.items()
+    }
     # Note: *_scale = min([|frame.delta| for frame in frames if |frame.delta| > 0.012])
-    x_scale, y_scale = _extract_delta_xy_from_gdcm_str(lines, dicom_tags) or (None, None)
+    x_scale, y_scale = _extract_delta_xy_from_gdcm_str(lines, dicom_tags) or (
+        None,
+        None,
+    )
     hr = _extract_hr_from_gdcm_str(lines, dicom_tags)
     nrow, ncol = _extract_xy_from_gdcm_str(lines, dicom_tags) or (None, None)
     # Note: returns frame_time (msec/frame) or 1000/cine_rate (frames/sec)
