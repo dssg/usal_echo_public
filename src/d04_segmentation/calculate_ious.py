bf3827df5b7053bc2df1e8710cfa0b8328a600f5
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-09 18:48:30 +0100

    redoing masks after resizing

--- src/d04_segmentation/calculate_ious.py
+++ src/d04_segmentation/calculate_ious.py
@@ -59,10 +59,10 @@ def main():
         
         #write to db
         # Evaluation Table: evaluation_id, instance_id, frame, chamber, study_id, score_type, score_value
-        df = pd.DataFrame({'instance_id' : gt_instance_id, 'frame' : gt['frame'], 
+        d = {'instance_id' : gt_instance_id, 'frame' : gt['frame'], 
               'chamber' : gt_chamber, 'study_id': gt_study_id, 
-              'score_type' : 'iou', 'score_value' : reported_iou})
-        io_segmentation.save_seg_evaluation_to_db(df)
+              'score_type' : 'iou', 'score_value' : reported_iou}
+        io_segmentation.save_seg_evaluation_to_db(pd.DataFrame(data=d))
         print('saved to db')
     
 
