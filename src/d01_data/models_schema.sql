4d148d98a366982c6295d72f1fd7bb9781b67a23
silil <liliana.millan@gmail.com>
2019-08-09 14:42:11 +0100

    classification schema

--- src/d01_data/models_schema.sql
+++ src/d01_data/models_schema.sql
@@ -1,3 +1,49 @@
+--CLASSIFICATION SCHEMA
+--tables for persisting the outputs of the measurement process
+DROP SCHEMA IF EXISTS classification CASCADE;
+
+CREATE SCHEMA classification;
+
+--table predictions:
+drop table if exists classification.predictions;
+
+create table classification.predictions(
+    prediction_id serial,
+    study_id integer,
+    instance_id integer,
+    file_name varchar,
+    model_name varchar,
+    date_run timestamp with time zone,
+    primary key(prediction_id)
+);
+
+
+--table evaluations
+drop table if exists classification.evaluations;
+
+create table classification.evaluations(
+    evaluation_id serial,
+    study_id integer,
+    instance_id integer,
+    file_name varchar,
+    score_type varchar,
+    score_value float,
+    primary key(evaluation_id)
+);
+
+--table ground_truths
+drop table if exists classification.ground_truths;
+
+create table classification.ground_truths(
+    ground_truth_id serial,
+    study_id integer,
+    instance_id integer,
+    file_name varchar,
+    view_name varchar,
+    primary key(ground_truth_id)
+);
+
+
 --SEGMENTATION SCHEMA
 ----tables for persisting the outputs of segmentation model
 
@@ -10,17 +56,18 @@ drop table if exists segmentation.predictions;
 
 create table segmentation.predictions(
    prediction_id serial,
-   instance_id integer,
    study_id integer,
+   instance_id integer,
+   file_name varchar,
+   frame integer,
    view_name varchar,
+   date_run timestamp with time zone,
    output_np_lv bytea,
    output_np_la bytea,
    output_np_lvo bytea,
    output_image_seg varchar,
    output_image_orig varchar,
    output_image_overlay varchar,
-   date_run timestamp with time zone,
-   file_name varchar,
    primary key(prediction_id)
 );
 
@@ -29,10 +76,10 @@ drop table if exists segmentation.evaluations;
 
 create table segmentation.evaluations(
     evaluation_id serial,
+    study_id integer,
     instance_id integer,
     frame integer,
     chamber varchar,
-    study_id integer,
     score_type varchar,
     score_value float,
     primary key(evaluation_id)
@@ -43,12 +90,12 @@ drop table if exists segmentation.ground_truths;
 
 create table segmentation.ground_truths(
     ground_truth_id serial,
+    study_id integer,
     instance_id integer,
     frame integer,
     chamber varchar,
-    study_id integer,
     view_name varchar,
-    numpy_array bytea[],
+    numpy_array bytea,
     primary key(ground_truth_id)
 );
 
