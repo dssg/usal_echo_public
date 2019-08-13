fdaab281136f0c449be90e383ed26d62344b6b89
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-13 16:05:02 +0100

    update create seg view code

--- src/d02_intermediate/create_seg_view.py
+++ src/d02_intermediate/create_seg_view.py
@@ -38,7 +38,7 @@ def create_seg_view():
 
     io_clean = dbReadWriteClean()
     io_views = dbReadWriteViews()
-
+    
     # 1. Import a_modvolume, a_measgraphref, a_measgraphic,
     #        measurement_abstract_rpt
     a_modvolume_df = io_clean.get_table("a_modvolume")
@@ -55,245 +55,68 @@ def create_seg_view():
     )
     del a_measgraphref_df
     del measurement_abstract_rpt_df
+    
+    #3. #create a dictionary to link measurements to views
 
-    # 3. Create one hot encoded columns for measurements of interest based
-    #        on measurement names
-    df["AVItd ap4"] = pd.np.where(df.name.str.contains("AVItd ap4"), True, False)
-    df["DVItd ap4"] = pd.np.where(df.name.str.contains("DVItd ap4"), True, False)
-    df["VTD(el-ps4)"] = pd.np.where(df.name.str.contains("VTD\(el-ps4\)"), True, False)
-    df["VTD(MDD-ps4)"] = pd.np.where(
-        df.name.str.contains("VTD\(MDD-ps4\)"), True, False
-    )
-
-    df["AVIts ap4"] = pd.np.where(df.name.str.contains("AVIts ap4"), True, False)
-    df["DVIts ap4"] = pd.np.where(df.name.str.contains("DVIts ap4"), True, False)
-    df["VTS(el-ps4)"] = pd.np.where(df.name.str.contains("VTS\(el-ps4\)"), True, False)
-    df["VTS(MDD-ps4)"] = pd.np.where(
-        df.name.str.contains("VTS\(MDD-ps4\)"), True, False
-    )
-
-    df["AVItd ap2"] = pd.np.where(df.name.str.contains("AVItd ap2"), True, False)
-    df["DVItd ap2"] = pd.np.where(df.name.str.contains("DVItd ap2"), True, False)
-    df["VTD(el-ps2)"] = pd.np.where(df.name.str.contains("VTD\(el-ps2\)"), True, False)
-    df["VTD(MDD-ps2)"] = pd.np.where(
-        df.name.str.contains("VTD\(MDD-ps2\)"), True, False
-    )
-
-    df["AVIts ap2"] = pd.np.where(df.name.str.contains("AVIts ap2"), True, False)
-    df["DVIts ap2"] = pd.np.where(df.name.str.contains("DVIts ap2"), True, False)
-    df["VTS(el-ps2)"] = pd.np.where(df.name.str.contains("VTS\(el-ps2\)"), True, False)
-    df["VTS(MDD-ps2)"] = pd.np.where(
-        df.name.str.contains("VTS\(MDD-ps2\)"), True, False
-    )
-
-    df["Vol. AI (MOD-sp4)"] = pd.np.where(
-        df.name.str.contains("Vol. AI \(MOD-sp4\)"), True, False
-    )
-    df["Vol. AI (MOD-sp2)"] = pd.np.where(
-        df.name.str.contains("Vol. AI \(MOD-sp2\)"), True, False
-    )
-
-    # 4. Create one shot encode columns for segment names based on one
-    #        hot encoded columns for measurements
-    df["A4C_Ven_ED"] = np.where(
-        (
-            (df["AVItd ap4"] == True)
-            | (df["DVItd ap4"] == True)
-            | (df["VTD(el-ps4)"] == True)
-            | (df["VTD(MDD-ps4)"] == True)
-        ),
-        True,
-        False,
-    )
-
-    df["A4C_Ven_ES"] = np.where(
-        (
-            (df["AVIts ap4"] == True)
-            | (df["DVIts ap4"] == True)
-            | (df["VTS(el-ps4)"] == True)
-            | (df["VTS(MDD-ps4)"] == True)
-        ),
-        True,
-        False,
-    )
-
-    df["A2C_Ven_ED"] = np.where(
-        (
-            (df["AVItd ap2"] == True)
-            | (df["DVItd ap2"] == True)
-            | (df["VTD(el-ps2)"] == True)
-            | (df["VTD(MDD-ps2)"] == True)
-        ),
-        True,
-        False,
-    )
-
-    df["A2C_Ven_ES"] = np.where(
-        (
-            (df["AVIts ap2"] == True)
-            | (df["DVIts ap2"] == True)
-            | (df["VTS(el-ps2)"] == True)
-            | (df["VTS(MDD-ps2)"] == True)
-        ),
-        True,
-        False,
-    )
-
-    df["A4C_Atr_ES"] = np.where((df["Vol. AI (MOD-sp4)"] == True), True, False)
-    df["A2C_Atr_ES"] = np.where((df["Vol. AI (MOD-sp2)"] == True), True, False)
-
-    # 5. Drop one hot encoded columns for measurements of interest
-    df.drop(
-        [
-            "AVItd ap4",
-            "DVItd ap4",
-            "VTD(el-ps4)",
-            "VTD(MDD-ps4)",
-            "AVIts ap4",
-            "DVIts ap4",
-            "VTS(el-ps4)",
-            "VTS(MDD-ps4)",
-            "AVItd ap2",
-            "DVItd ap2",
-            "VTD(el-ps2)",
-            "VTD(MDD-ps2)",
-            "AVIts ap2",
-            "DVIts ap2",
-            "VTS(el-ps2)",
-            "VTS(MDD-ps2)",
-            "Vol. AI (MOD-sp4)",
-            "Vol. AI (MOD-sp2)",
-        ],
-        axis=1,
-        inplace=True,
-    )
-
-    # 6. Cut the dataframe to relevant columns as drop duplicates
-    df_1 = df[
-        [
-            "studyidk",
-            "instanceidk",
-            "indexinmglist",
-            "A4C_Ven_ED",
-            "A4C_Ven_ES",
-            "A2C_Ven_ED",
-            "A2C_Ven_ES",
-            "A4C_Atr_ES",
-            "A2C_Atr_ES",
-        ]
-    ].copy()
+    measurement_2_view_dict = {'AVItd ap4' :'A4C', 'DVItd ap4' : 'A4C', 'VTD(el-ps4)': 'A4C', 'VTD(MDD-ps4)': 'A4C', 
+                               'AVIts ap4': 'A4C', 'DVIts ap4':'A4C', 'VTS(el-ps4)':'A4C', 'VTS(MDD-ps4)':'A4C', 
+                               'AVItd ap2':'A2C', 'DVItd ap2':'A2C', 'VTD(el-ps2)':'A2C', 'VTD(MDD-ps2)':'A2C', 
+                               'AVIts ap2':'A2C', 'DVIts ap2':'A2C', 'VTS(el-ps2)': 'A2C', 'VTS(MDD-ps2)':'A2C', 
+                               'Vol. AI (MOD-sp4)':'A4C', 'Vol. AI (MOD-sp2)':'A2C'}
+    
+    measurement_2_chamber_dict = {'AVItd ap4' :'lv', 'DVItd ap4' : 'lv', 'VTD(el-ps4)': 'lv', 'VTD(MDD-ps4)': 'lv', 
+                               'AVIts ap4': 'lv', 'DVIts ap4':'lv', 'VTS(el-ps4)':'lv', 'VTS(MDD-ps4)':'lv', 
+                               'AVItd ap2':'lv', 'DVItd ap2':'lv', 'VTD(el-ps2)':'lv', 'VTD(MDD-ps2)':'lv', 
+                               'AVIts ap2':'lv', 'DVIts ap2':'lv', 'VTS(el-ps2)': 'lv', 'VTS(MDD-ps2)':'lv', 
+                               'Vol. AI (MOD-sp4)':'la', 'Vol. AI (MOD-sp2)':'la'}
+    
+    measurement_2_cardio_moment_dict = {'AVItd ap4' :'ED', 'DVItd ap4' : 'ED', 'VTD(el-ps4)': 'ED', 'VTD(MDD-ps4)': 'ED', 
+                               'AVIts ap4': 'ES', 'DVIts ap4':'ES', 'VTS(el-ps4)':'ES', 'VTS(MDD-ps4)':'ES', 
+                               'AVItd ap2':'ED', 'DVItd ap2':'ED', 'VTD(el-ps2)':'ED', 'VTD(MDD-ps2)':'ED', 
+                               'AVIts ap2':'ES', 'DVIts ap2':'ES', 'VTS(el-ps2)': 'ES', 'VTS(MDD-ps2)':'ES', 
+                               'Vol. AI (MOD-sp4)':'ES', 'Vol. AI (MOD-sp2)':'ES'}
+    
+    #4. # Add these dictionaries to the df_2
+    df_2 = df
+    df_2['view'] = df['name'].map(measurement_2_view_dict)
+    df_2['chamber'] = df['name'].map(measurement_2_chamber_dict)
+    df_2['cardio_moment'] = df['name'].map(measurement_2_cardio_moment_dict)
+    
     del df
-    df_2 = df_1.drop_duplicates()
-    del df_1
-
-    #  7. merge with a_measgraphic_df for frame number
-    df_3 = pd.merge(
-        df_2, a_measgraphic_df, how="left", on=["instanceidk", "indexinmglist"]
-    )
+    
+    #5# Cut out the rows without view, chamber and cadio_moment
+    df_3 = df_2[pd.notna(df_2['view']) == True]
     del df_2
-    del a_measgraphic_df
-
-    # 8. Cut the dataframe to relevant columns as drop duplicates
-    df_4 = df_3[
-        [
-            "studyidk",
-            "instanceidk",
-            "indexinmglist",
-            "A4C_Ven_ED",
-            "A4C_Ven_ES",
-            "A2C_Ven_ED",
-            "A2C_Ven_ES",
-            "A4C_Atr_ES",
-            "A2C_Atr_ES",
-            "frame",
-        ]
-    ].copy()
-    del df_3
-
-    # 9. merge with a_modvolume
-    df_5 = pd.merge(
-        a_modvolume_df, df_4, how="left", on=["instanceidk", "indexinmglist"]
-    )
+    
+    #6.  dropping -1 instances
+    df_4 = df_3[df_3['instanceidk'] != -1]
+    
+    #7. Only colums we need &drop duplicates
+    df_5 = df_4[['studyidk', 'indexinmglist', 'instanceidk', 'frame','view',
+       'chamber', 'cardio_moment']].copy()
+    df_6 = df_5.drop_duplicates()
+    
     del df_4
-    del a_modvolume_df
-
-    # 10. Drop instance id 1789286 and 3121715 due to problems linking and
-    #        conflicts
-    #   Drop unnessecary row_id from orginal tables (causes problems in dbWrite)
-    df_6 = df_5.drop(
-        df_5[(df_5.instanceidk == 1789286) | (df_5.instanceidk == 3121715)].index
-    )
     del df_5
 
-    df_6.columns = map(str.lower, df_6.columns)
-    
-    # New transformations added later - may need to revise scripts to make more
-    # efficient :see git issue
-    # 11. melt the table to a single field for the views
-    df_7 = pd.melt(
-        df_6,
-        id_vars=[
-            "instanceidk",
-            "indexinmglist",
-            "chordsequence",
-            "chordtype",
-            "x1coordinate",
-            "y1coordinate",
-            "chordlength",
-            "x2coordinate",
-            "y2coordinate",
-            "interchorddistance",
-            "studyidk",
-            "frame",
-        ],
-        value_vars=[
-            "a4c_ven_ed",
-            "a4c_ven_es",
-            "a2c_ven_ed",
-            "a2c_ven_es",
-            "a4c_atr_es",
-            "a2c_atr_es",
-        ],
-        var_name="view_name",
-        value_name="view_exists",
+    # 8. Drop instance id 1789286 and 3121715 due to problems linking and
+    #        conflicts
+    #   Drop unnessecary row_id from orginal tables (causes problems in dbWrite)
+    df_7 = df_6.drop(
+        df_6[(df_6.instanceidk == 1789286) | (df_6.instanceidk == 3121715)].index
     )
     del df_6
     
-    #filter to only views that exist
-    df_7 = df_7[df_7['view_exists'] == True]
-    
-    # write output to db    
-    io_views.save_to_db(df_7, "chords_by_volume_mask")
+    # 9. Saving to frames_by_volume_mask
+    df_7.columns = map(str.lower, df_7.columns)
+    io_views.save_to_db(df_7, "frames_by_volume_mask")
 
-
-    # 12. drop chord sequence and remove duplicates
-    df_8 = df_7.drop(
-        [
-            "chordsequence",
-            "chordtype",
-            "x1coordinate",
-            "y1coordinate",
-            "chordlength",
-            "x2coordinate",
-            "y2coordinate",
-            "interchorddistance",
-        ],
-        axis=1,
+    # 9. merge with a_modvolume
+    df_8 = pd.merge(
+        a_modvolume_df, df_7, how="left", on=["instanceidk", "indexinmglist"]
     )
-    df_9 = df_8.drop_duplicates()
     del df_7
-    del df_8
-
-    # 13. create detailed columns
-    # create view column
-    df_9["view_only"] = df_9["view_name"].apply(lambda x: str(x)[:3])
-    # create ventricle column
-    df_9["ventricle_only"] = df_9["view_name"].apply(lambda x: str(x)[4:7])
-    # create cycle column
-    df_9["cycle_only"] = df_9["view_name"].apply(lambda x: str(x)[8:10])
-
-    # 14. # reset index
-    df_9.reset_index()
-
-    # write output to db
-    io_views.save_to_db(df_9, "frames_by_volume_mask")
+    del a_modvolume_df
+    
+    io_views.save_to_db(df_8, "chords_by_volume_mask")
+    
