fc9e8cf243337a7ac5f7d6eb23f253a0edb174f6
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-13 17:33:44 +0100

    update generate masks code to work with new tables

--- src/d04_segmentation/generate_masks.py
+++ src/d04_segmentation/generate_masks.py
@@ -27,7 +27,7 @@ def write_masks():
         print('Orginal numpy array size: {}'.format(mask['mask'].shape))
         resized_mask = (imresize(mask['mask'], (384, 384)))
         print('Revised numpy array size: {}'.format(resized_mask.shape))
-        d = [mask['studyidk'], mask['instanceidk'], mask['instancefilename'], 
+        d = [int(mask['studyidk']), mask['instanceidk'], mask['instancefilename'], 
              mask['frame'], mask['chamber'], mask['view'], resized_mask]
         
         io_segmentation.save_ground_truth_numpy_array_to_db(d, gt_table_column_names)
