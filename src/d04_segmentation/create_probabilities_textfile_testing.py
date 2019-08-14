426c3526fa0aeb936e4158e8183db28981017bf2
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-14 18:00:53 +0100

    changes to the create probabilities textfile script

--- src/d04_segmentation/create_probabilities_textfile_testing.py
+++ src/d04_segmentation/create_probabilities_textfile_testing.py
@@ -30,7 +30,8 @@ def create_probabilities_textfile_testing():
     :param: instanceids: a list of instance ids
     :return nothing
     """
-
+    
+    '''
     # 1. Gets frames_by_volume_mask view
     io_views = dbReadWriteViews()
     frames_by_volume_mask = io_views.get_table("frames_by_volume_mask")
@@ -44,12 +45,12 @@ def create_probabilities_textfile_testing():
     print('instance_list size: {}'.format(instances_unique_master_list.shape))
     
     # 2. filter frames_by_volume_mask to only include instances_unique_master_list
-    df = pd.merge(instances_unique_master_list, frames_by_volume_mask, how="left", 
+    #df = pd.merge(instances_unique_master_list, frames_by_volume_mask, how="left", 
                   on=['studyidk', 'instanceidk'])
+    #print("df shape:{}".format(df.shape))
+    
+    df = frames_by_volume_mask[frames_by_volume_mask['instanceidk'].isin(instances_unique_master_list['instanceidk'])]
     print("df shape:{}".format(df.shape))
-    
-    #frames_by_volume_mask[frames_by_volume_mask['instanceidk'].isin(instances_unique_master_list['instanceidk'])]
-    
     #3. Drop instance index, and delete any duplicates
     #df = df.drop(columns=['indexinmglist'])
     #df = df.drop_duplicates()
@@ -113,3 +114,64 @@ def create_probabilities_textfile_testing():
     data_path = os.path.join(data_dir, file_name)
 
     df_3.to_csv(data_path, index=None, header=True, sep="\t")
+'''
+
+    instances_w_labels_test_downsampleby5 = io_views.get_table("instances_w_labels_test_downsampleby5")
+    prob_tb = pd.DataFrame(
+    columns=[
+        "study",
+        "image",
+        "plax_far",
+        "plax_plax",
+        "plax_laz",
+        "psax_az",
+        "psax_mv",
+        "psax_pap",
+        "a2c_lvocc_s",
+        "a2c_laocc",
+        "a2c",
+        "a3c_lvocc_s",
+        "a3c_laocc",
+        "a3c",
+        "a4c_lvocc_s",
+        "a4c_laocc",
+        "a4c",
+        "a5c",
+        "other",
+        "rvinf",
+        "psax_avz",
+        "suprasternal",
+        "subcostal",
+        "plax_lac",
+        "psax_apex",
+    ])
+    
+    for i in instances_w_labels_test_downsampleby5.index:
+        prob_tb.at[i, "study"] = "/home/ubuntu/data/01_raw/test_downsampleby5"
+        filename = instances_w_labels_test_downsampleby5.at[i, "filename"]
+        studyidk = instances_w_labels_test_downsampleby5.at[i, "studyidk"]
+        prob_tb.at[i, "image"] = (
+        "a_" + str(studyidk).strip() + "_" + str(filename).strip() + ".dcm"
+        )
+        if instances_w_labels_test_downsampleby5.at[i, "view"] =="a4c":
+            prob_tb.at[i, "a4c"] = 0.9898
+        elif instances_w_labels_test_downsampleby5.at[i, "view"] =="a2c":
+            prob_tb.at[i, "a2c"] = 0.9898
+    
+    df_3 = prob_tb.fillna(0)
+    print('df_3: {}'.format(df_3.shape))
+
+    # 5. Writes the probabilities textfile to data/d04_segmentation folder
+    project_dir = os.path.dirname(os.getcwd())
+    data_dir = os.path.join(project_dir, "data", "d04_segmentation")
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    time_stamp = datetime.date.today()
+    file_name = "view_probabilities_test" + str(time_stamp) + ".txt"
+    data_path = os.path.join(data_dir, file_name)
+
+    df_3.to_csv(data_path, index=None, header=True, sep="\t")
+        
+        
--- src/d04_segmentation/segment_view.py
+++ src/d04_segmentation/segment_view.py
@@ -319,7 +319,7 @@ def main():
     global dicomdir
     # In case dicomdir is path with more than one part.
     # dicomdir_basename = os.path.basename(dicomdir)
-    viewfile = "/home/ubuntu/courtney/usal_echo/data/d04_segmentation/view_probabilities_test2019-08-13.txt"
+    viewfile = "/home/ubuntu/courtney/usal_echo/data/d04_segmentation/view_probabilities_test2019-08-14.txt"
     # viewfile = '/home/ubuntu/courtney/usal_echo/data/d04_segmentation/view_23_e5_class_11-Mar-2018_dcm_sample_labelled_probabilities.txt'
     viewlist_a2c = []
     viewlist_a3c = []
