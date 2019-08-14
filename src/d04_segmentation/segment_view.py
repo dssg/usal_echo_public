8aff99ee77132e31d8cc6057a68d03c875f00d41
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-14 11:50:07 +0100

    debug

--- src/d04_segmentation/segment_view.py
+++ src/d04_segmentation/segment_view.py
@@ -16,7 +16,7 @@ import hashlib
 
 #from d00_utils.echocv_utils_v0 import *
 #from d02_intermediate.download_dcm import download_dcm
-from d00_utils.db_utils import dcm_to_segmentation_arrays
+#from d00_utils.db_utils import dcm_to_segmentation_arrays
 from d00_utils.db_utils import dbReadWriteViews, dbReadWriteSegmentation
 from d04_segmentation.model_unet import Unet
 
