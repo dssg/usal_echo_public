110a355e9eaaf8d9a87af2c0b5ad5b3bab876e57
courtneyjean84 <courtney_irwin@hotmail.com>
2019-08-19 19:33:16 +0100

    debug

--- src/d04_segmentation/evaluate_masks.py
+++ src/d04_segmentation/evaluate_masks.py
@@ -46,7 +46,7 @@ def evaluate_masks():
         if len(pred.index) > 0:
             pred_view_name = gt['view_name']
             #retrieve gt numpy array
-            gt_numpy_array = io_segmentation.convert_to_np(gt['numpy_array'], 1)#frame = 1, as it wants number of frames in np array, not frame number
+            gt_numpy_array = io_segmentation.convert_to_np(gt['numpy_array'][0], 1)#frame = 1, as it wants number of frames in np array, not frame number
             if gt_numpy_array == None:
                 continue
             #retrive relevant pred numpy array
